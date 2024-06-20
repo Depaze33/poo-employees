172ac4da9a9a7d6d6aee1af4dd876afb96a91394
Clarisse Otmani <clarisseotm@gmail.com>
2024-06-20 17:10:01 +0200

    Modification de l'exercice sur la POO

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,5 +1,8 @@
 package fr.afpa.employees;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /*
 Objectif :
 L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
@@ -9,31 +12,100 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+class Employee {
 	/**
 	 * Matricule de l'employé
 	 */
+	// Definir les attribut
 	private String registrationNumber;
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private LocalDate birthDay;
+	private final int socialRate = 30;
 
-	// TODO compléter les attributs comme présenté dans le PDF
-
-	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	// compléter les attributs comme présenté dans le PDF
+	// Appeler les objet
+	// compléter le constructeur de la classe
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthDay) {
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDay = LocalDate.parse(birthDay);
+
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs privés)
+	// decalare les getters
 	public String getRegistrationNumber() {
 		return registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public LocalDate getBirthDay() {
+		return birthDay;
+	}
+
+	// declare les setters
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
-	// TODO ajouter les getters et les setters
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public void setBirthDay(LocalDate birthDay) {
+		this.birthDay = birthDay;
+	}
+
+	// implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Employee { "
+				+ " \nMatricule " + getRegistrationNumber()
+				+ " \nNom " + getLastName()
+				+ " \nPrenom " + getFirstName()
+				+ " \nSalaire " + getSalary()
+				+ " \nDate d'anniversaire " + getBirthDay()
+				+ "\nSalaire net " + netSalary()
+				+ '}';
+
+	}
+
+	// methode pour calcul salire net
+	public double netSalary() {
+		
+		return this.salary - this.salary * (this.socialRate/100.0);
+	}
+	public long  daysBeforeBirthday() {
+		LocalDate currentDate = LocalDate.now();
+		newDate = (LocalDate.now() - birthDay)
+		return ChronoUnit.DAYS.between(LocalDate.now(), birthDay-LocalDate.now());  ;
+	}
 }
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -8,14 +8,27 @@ import java.time.temporal.ChronoUnit;
  */
 class EmployeeMain
 {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) 
 	{
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		// instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
+		Employee employee1 = new Employee("11ABC22, ", "Sari", "Farah", 1500.50, "1985-02-25" );
+		Employee employee2 = new Employee("12ABC23, ", "Atmane", "Sara", 1300.20, "1996-02-05");
+		Employee employee3 = new Employee("13ABC24, ", "Laurent", "Patrick", 1800.20, "1996-02-05");
+		Employee employee4 = new Employee("14ABC25, ", "Prevoteau", "Jean-Pierre", 2000.0, "1996-02-05");
 
-		// TODO afficher les informations des employés avec System.out.println
+		System.out.println(employee1);
+		 
+		// afficher les informations des employés avec System.out.println
+		 System.out.println(employee1.toString());
+		 System.out.println(employee2.toString());
+		 System.out.println(employee3.toString());
+		 System.out.println(employee4.toString());
+		 
 	}
 }
 
